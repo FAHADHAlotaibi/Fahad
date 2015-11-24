376ea8eb3afa6cd60101803eb133509cabaff476
Fahad Hammad Salem Alotaibi <fhd.alotaibi@hotmail.com>
2015-11-24 06:00:04 +0300

    added initGUI

--- app/src/main/java/fahad/com/gittest/MainActivity.java
+++ app/src/main/java/fahad/com/gittest/MainActivity.java
@@ -28,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void initGUI(){
+
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
